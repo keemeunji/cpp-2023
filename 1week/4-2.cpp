6a1fc57b7a4d9b2040a7d16954f82325f9e00987
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2023-09-26 18:34:09 +0900

    Create 4-2.cpp

---
+++ 1week/4-2.cpp
@@ -0,0 +1,19 @@
+/* while문 예제 
+4-1의 for문을 while문으로 바꾸어 작성하라. */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i, a, b, sum=0;
+  cout << "두 개의 정수 입력 >>";
+  cin >> a >> b;
+
+  i=a;
+  while(i<=b) {  // i가 b보다 작거나 같은 동안 반복
+    sum += i;
+    i++;
+  }
+
+  cout << a << "에서 " << b << "까지 합은 " << sum;
+}
