559ef6f330b8c7660af440c5bebc877152b3a8ee
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2023-10-11 00:03:09 +0900

    Create 3-4.cpp

---
+++ 2week/3-4.cpp
@@ -0,0 +1,32 @@
+// 생성자에서 다른 생성자 호출 연습 (위임 생성자 만들기)
+#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+  int radius;
+  Circle();  // 위임 생성자
+  Circle(int r);  // 타겟 생성자
+  double getArea();
+};
+
+Circle::Circle() : Circle(1) { }  // 위임 생성자
+
+Circle::Circle(int r) {  // 타겟 생성자
+  radius = r;
+  cout << "반지름이 " << radius << "인 원 생성" << endl;
+}
+
+double Circle::getArea() {
+  return 3.14*radius*radius;
+}
+
+int main() {
+  Circle donut;
+  double area = donut.getArea();
+  cout << "donut의 면적은 " << area << endl;
+
+  Circle pizza(30);
+  area = pizza.getArea();
+  cout << "pizza의 면적은 " << area << endl;
+}
