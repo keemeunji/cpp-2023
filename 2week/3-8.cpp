e7a1caed1617d463e3132f74fda948757d5b0f43
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2023-10-11 20:20:52 +0900

    Create 3-8.cpp

---
+++ 2week/3-8.cpp
@@ -0,0 +1,44 @@
+// 지역 객체와 전역 객체의 생성 및 소멸 순서
+#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+  int radius;
+  Circle();
+  Circle(int r);
+  ~Circle();
+  double getArea();
+};
+
+Circle::Circle() {
+  radius = 1;
+  cout << "반지름이 " << radius << "인 원 생성" << endl;
+}
+
+Circle::Circle(int r) {
+  radius = r;
+  cout << "반지름이 " << radius << "인 원 생성" << endl;
+}
+
+Circle::~Circle() {
+  cout << "반지름이 " << radius << "인 원 소멸" << endl;
+}
+
+double Circle::getArea() {
+  return 3.14*radius*radius;
+}
+
+Circle globalDonut(1000);  // 전역 객체 생성
+Circle globalPizza(2000);
+
+void f() {  // 지역 객체 생성
+  Circle fDonut(100);
+  Circle fPizza(200);
+}
+
+int main() {  // 지역 객체 생성
+  Circle mainDonut;
+  Circle mainPizza(30);
+  f();
+}
