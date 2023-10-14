b4597f02a930eeaf6b696e748943787ca2342b72
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2023-10-14 18:05:31 +0900

    Create 4-15.cpp

---
+++ 3week/4-15.cpp
@@ -0,0 +1,27 @@
+// 문자열 find 및 replace
+// &가 입력될 때까지 여러 줄의 영문 문자열을 입력 받고, 찾는 문자열과 대치할 문자열을 각각 입력받아 문자열을 변경하라.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cout << "여러 줄의 문자열을 입력하세요. 입력의 끝은 & 문자입니다." << endl;
+  getline(cin, s, '&');
+  cin.ignore();  // & 뒤에 따라 오는 <Enter> 키를 제거하기 위한 코드
+  string f, r;
+  cout << endl << "find: ";
+  getline(cin, f, '\n');  // 검색할 문자열 입력
+  cout << "replace: ";
+  getline(cin, r, '\n');  // 대치할 문자열 입력
+
+  int startIndex = 0;
+  while(true) {
+    int fIndex = s.find(f, startIndex);  // startIndex부터 문자열 f 검색
+    if(fIndex == -1)
+      break;  // 문자열 s의 끝까지 변경하였음
+    s.replace(fIndex, f.length(), r);  // fIndex부터 문자열 f의 길이만큼 문자열 r로 변경
+    startIndex = fIndex + r.length();
+  }
+  cout << s << endl;
+}
