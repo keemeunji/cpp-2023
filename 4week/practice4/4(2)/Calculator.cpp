4fc6ad93ddfdb70fb4c868fc3508cd1ea8c68207
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2023-10-15 17:31:56 +0900

    Update Calculator.cpp

--- 4week/practice4/4(2)/Calculator.cpp
+++ 4week/practice4/4(2)/Calculator.cpp
@@ -12,3 +12,32 @@ using namespace std;
   int Sub::calculate() { return a - b; }
   int Mul::calculate() { return a * b; }
   int Div::calculate() { return a / b; }
+
+int main() {
+  Add a;
+  Sub s;
+  Mul m;
+  Div d;
+  int num1, num2;
+  char oper;
+  while(true) {
+    cout << "두 정수와 연산자를 입력하세요>> ";
+    cin >> num1 >> num2 >> oper;
+    if (oper == '+') {
+      a.setValue(num1, num2);
+      cout << a.calculate() <<endl;
+    }
+    else if (oper == '-') {
+      s.setValue(num1, num2);
+      cout << s.calculate() <<endl;
+    }
+    else if (oper == '*') {
+      m.setValue(num1, num2);
+      cout << m.calculate() <<endl;
+    }
+    else if (oper == '/') {
+      d.setValue(num1, num2);
+      cout << d.calculate() <<endl;
+    }
+  }
+}
