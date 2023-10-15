2b0bf7ffdbe4c4b99255fdf40dd959e9800754ad
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2023-10-15 17:25:55 +0900

    Create Calculator.h

---
+++ 4week/practice4/4(2)/Calculator.h
@@ -0,0 +1,27 @@
+class Add {
+  int a, b;
+public:
+  void setValue(int x, int y);
+  int calculate();
+};
+
+class Sub {
+  int a, b;
+public:
+  void setValue(int x, int y);
+  int calculate();
+};
+
+class Mul {
+  int a, b;
+public:
+  void setValue(int x, int y);
+  int calculate();
+};
+
+class Div {
+  int a, b;
+public:
+  void setValue(int x, int y);
+  int calculate();
+};
