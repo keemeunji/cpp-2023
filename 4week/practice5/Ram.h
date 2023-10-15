ec3c7116fc2d0c0791525e92d24693b43c301fbf
Kim Eunji <117907077+keemeunji@users.noreply.github.com>
2023-10-15 17:59:10 +0900

    Update Ram.h

--- 4week/practice5/Ram.h
+++ 4week/practice5/Ram.h
@@ -1,25 +1,5 @@
 /* 컴퓨터의 주기억장치를 모델링하는 클래스 Ram을 구현하려고 한다. Ram 클래스는 데이터가 기록될 메모리 공간과 크기 정보를 가지고,
 주어진 주소에 데이터를 기록하고(write), 주어진 주소로부터 데이터를 읽어온다.(read). 
-- Ram 클래스는 다음과 같이 선언된다.
-class Ram {
-  char mem[100 * 1024];
-  int size;
-public:
-  Ram();
-  ~Ram();
-  char read(int address);
-  void write(int address, char value);
-};
-
-- 다음 main() 함수는 100번지에 20을 저장하고, 101번지에 30을 저장한 후, 100번지와 101번지의 값을 읽고 더하여 102번지에 저장하는 코드이다.
-int main() {
-  Ram ram;
-  ram.write(100, 20);
-  ram.write(101, 30);
-  char res = ram.read(100) + ram.read(101);
-  ram.write(102,res);
-  cout << "102번지의 값 = " << (int)ram.read(102) << endl;
-}
 
 실행결과
 102번지의 값 = 50
